8b52e7817d81ab4655449b44555fe92293295080
Cinemavillaz <87010768+Cinemavillaz@users.noreply.github.com>
2021-09-12 17:34:04 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -91,7 +91,7 @@ def get_reply_markup(query):
     buttons = [
         [
             InlineKeyboardButton('Search again', switch_inline_query_current_chat=query),
-            InlineKeyboardButton('More Bots', url='https://t.me/subin_works/122')
+            InlineKeyboardButton('𝐔𝐏𝐃𝐀𝐓𝐄𝐒 ⚒️', url='https://t.me/cv_updatez')
         ]
         ]
     return InlineKeyboardMarkup(buttons)
