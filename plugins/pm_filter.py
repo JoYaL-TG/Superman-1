20b2d8afc0a04890dba6969f39103e7826805645
Joel Bx <87010768+Cinemavillaz@users.noreply.github.com>
2021-10-13 13:05:38 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -6,8 +6,7 @@ import re
 import random
 import asyncio
 from pyrogram.errors import UserNotParticipant
-from utils import get_filter_results,
-fromt_file_details, is_subscribed, get_poster
+from utils import get_filter_results, get_file_details, is_subscribed, get_poster
 BUTTONS = {}
 BOT = {}
 
