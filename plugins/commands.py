f3e71a1dcea728e59caf6fb6608b420785359c6d
Mufaz-TG <92558762+Mufaz-TG@users.noreply.github.com>
2021-10-18 12:04:40 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -61,7 +61,7 @@ async def text(client, message):
 @Client.on_message(filters.command(['start']))
 async def start(client, message):
        await message.reply(
-    usr_cmdall1 = cmd.text
+        usr_cmdall1 = cmd.text
     if usr_cmdall1.startswith("/start subinps"):
         if AUTH_CHANNEL:
             invite_link = await bot.create_chat_invite_link(int(AUTH_CHANNEL))
