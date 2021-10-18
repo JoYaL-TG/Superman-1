4ad1eb5724d555f01a0afe603b5855a0106a3c89
Mufaz-TG <92558762+Mufaz-TG@users.noreply.github.com>
2021-10-18 11:54:19 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -54,7 +54,7 @@ PHOTO = [
     "https://telegra.ph/file/50f89bfd69dc6ceff0cb9.jpg"
 ]
 
-@Client.on_message(filters.all & filters.private | filters.group ))
+@Client.on_message(filters.all & filters.private | filters.group )
 async def text(client, message):
        (int(message.chat.id))
 
