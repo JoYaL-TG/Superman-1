7a82ef1d598d042855ba4a1164416ee7a3d68543
JoYaL-TG <98303967+JoYaL-TG@users.noreply.github.com>
2022-06-13 09:26:25 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -101,10 +101,10 @@ async def filter(client, message):
             if API_KEY:
                 poster=await get_poster(search)
             if poster:
-                await message.reply_photo(photo=poster, caption=("PICA"), reply_markup=InlineKeyboardMarkup(buttons))
+                await message.reply_photo(photo=poster, caption=(PICA), reply_markup=InlineKeyboardMarkup(buttons))
 
             else:
-                await message.reply_text("PICA"), reply_markup=InlineKeyboardMarkup(buttons))
+                await message.reply_text(PICA), reply_markup=InlineKeyboardMarkup(buttons))
             return
 
         data = BUTTONS[keyword]
@@ -122,7 +122,7 @@ async def filter(client, message):
         if poster:
             await message.reply_photo(photo=poster, caption=f"<b>Here is What I Found In My Database For Your Query {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  </b>", reply_markup=InlineKeyboardMarkup(buttons))
         else:
-            await message.reply_text("PICA"), reply_markup=InlineKeyboardMarkup(buttons))
+            await message.reply_text(PICA), reply_markup=InlineKeyboardMarkup(buttons))
 
 @Client.on_message(filters.text & filters.group & filters.incoming & filters.chat(AUTH_GROUPS) if AUTH_GROUPS else filters.text & filters.group & filters.incoming)
 async def group(client, message):
@@ -168,7 +168,7 @@ async def group(client, message):
             if API_KEY:
                 poster=await get_poster(search)
             if poster:
-                await message.reply_photo(photo=poster, caption=("PICA"), reply_markup=InlineKeyboardMarkup(buttons))
+                await message.reply_photo(photo=poster, caption=(PICA), reply_markup=InlineKeyboardMarkup(buttons))
             else:
                 await message.reply_text(f"**🎬 Title:** {search}\n**🌟 Rating:** {random.choice(RATING)}\n**🎭 Genre:** {random.choice(GENRES)}\n**🗳️ Votes:** {random.choice(VOTES)}\n**©️ {message.chat.title} 🍿**", reply_markup=InlineKeyboardMarkup(buttons))
             return
@@ -186,7 +186,7 @@ async def group(client, message):
         if API_KEY:
             poster=await get_poster(search)
         if poster:
-            await message.reply_photo(photo=poster, caption=("PICA"), reply_markup=InlineKeyboardMarkup(buttons))
+            await message.reply_photo(photo=poster, caption=(PICA), reply_markup=InlineKeyboardMarkup(buttons))
         else:
             await message.reply_text(f"**🎬 Title:** {search}\n**🌟 Rating:** {random.choice(RATING)}\n**🎭 Genre:** {random.choice(GENRES)}\n**🗳️ Votes:** {random.choice(VOTES)}\n**©️ {message.chat.title} 🍿**", reply_markup=InlineKeyboardMarkup(buttons))
 
